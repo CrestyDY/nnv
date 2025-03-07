889b6a7e803a20ecc150f7d75b3c61be91c65ac9
CrestyDY <danielyumtl@hotmail.com>
2025-03-07 10:46:21 -0500

    tag manager

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -194,7 +194,6 @@ function App() {
 
     return (
         <div>
-            <!-- Google tag (gtag.js) -->
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-0PN7PG4DSJ"></script>
             <script>
                 window.dataLayer = window.dataLayer || [];
